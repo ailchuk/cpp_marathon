6f3d8a000873d45917cbc5da4025b508f618d8f3
Andrii Ilchuk <ailchuk@e2r13p9.unit.ua>
2020-08-21 17:00:21 +0300

    t00.1

--- sp04/t00/app/main.cpp
+++ sp04/t00/app/main.cpp
@@ -3,8 +3,10 @@
 #include "algorithmUtils.h"
 
 int main() {
-  // std::cout << "IsInsideRange\n"
-  //           << "======================================================\n";
+  // std::cout << "IsInRange\n" << "======================================================\n";
+  // std::cout << "IsInRange\n" << "======================================================\n";
+
+  // std::cout << "IsInsideRange\n" << "======================================================\n";
   // IsInsideRange(30, 10, 100) ? std::cout << "Yes\n" : std::cout << "No\n";
   // IsInsideRange(5, 10, 100) ? std::cout << "Yes\n" : std::cout << "No\n";
 
@@ -12,33 +14,84 @@ int main() {
   // IsInsideRange(5, {10, 100}) ? std::cout << "Yes\n" : std::cout << "No\n";
   // std::cout << "======================================================\n";
 
-  // std::cout << "Modify\n"
-  //           << "======================================================\n";
+  // std::cout << "Modify\n" << "======================================================\n";
   // std::string a = "Aproweqwe";
   // std::cout << "[Obj: " << a << "]\n";
   // Modify(a, "Modified") ? std::cout << a << '\n': std::cout << "Bad\n";
   // Modify(a, "Second Modify") ? std::cout << a << '\n' : std::cout << "Bad\n";
   // std::cout << "======================================================\n";
 
-  // std::cout << "Find\n"
-  //           << "======================================================\n";
+  // std::cout << "RemoveAll\n" << "======================================================\n";
+  // std::vector<int> i = {1, 2, 3, 4, 4, 4, 4, 8, 9, 10};
+  // std::vector<int> b = {1, 2, 3, 4, 4, 4, 4, 8, 9, 10};
+
+  // for (const auto &a : i)
+  //   std::cout << a << " ";
+  // RemoveAll(i, 4);
+  // std::cout << "\nremoved: ";
+  // for (const auto &a : i)
+  //   std::cout << a << " ";
+  // std::cout << "\n======================================================\n";
+
+  // std::cout << "RemoveAllIf\n" << "======================================================\n";
+  // for (const auto &a : b)
+  //   std::cout << a << " ";
+  // RemoveAllIf(b, [](auto i) { return ((i % 2) != 1); });
+  // std::cout << "\nremoved: ";
+  // for (const auto &a : b)
+  //   std::cout << a << " ";
+  // std::cout << "======================================================\n";
+
+  // std::cout << "Find\n" << "======================================================\n";
   // std::vector<int> v = {1, 22, 3, 123, 14};
   // std::cout << Find(v, 144);
   // std::cout << *std::find(v.begin(), v.end(), 144);
   // std::cout << "======================================================\n";
 
-//   std::cout << "FindIf\n"
-//             << "======================================================\n";
-//   std::vector<int> v = {10, 25, 40, 55};
-//   std::cout << *FindIf(v, [](auto i) { return ((i % 2) == 1); }) << '\n';
-//   std::cout << *std::find_if(v.begin(), v.end(), [](auto i) { return ((i % 2) == 1); } );
-//   std::cout << "\n======================================================\n";
+  // std::cout << "FindIf\n" << "======================================================\n";
+  //   std::vector<int> v = {10, 25, 40, 55};
+  //   std::cout << *FindIf(v, [](auto i) { return ((i % 2) == 1); }) << '\n';
+  //   std::cout << *std::find_if(v.begin(), v.end(), [](auto i) { return ((i % 2) == 1); } );
+  //   std::cout << "\n======================================================\n";
 
-  std::cout << "Contains\n"
+  // std::cout << "Contains\n" << "======================================================\n";
+  // std::vector<int> v = {10, 25, 40, 55};
+  // Contains(v, 20) ? std::cout << "Contains\n" : std::cout << "Not contains\n";
+  // (std::find(v.begin(), v.end(), 20) != v.end()) ? std::cout << "Std Contains\n" : std::cout << "Std Not contains\n";
+  // Contains(v, 10) ? std::cout << "Contains\n" : std::cout << "Not contains\n";
+  // (std::find(v.begin(), v.end(), 10) != v.end()) ? std::cout << "Std Contains\n" : std::cout << "Std Not contains\n";
+  // std::cout << "======================================================\n\n";
+
+  // std::cout << "ContainsIf\n" << "======================================================\n";
+  // // std::vector<int> v = {10, 25, 40, 55};
+  // ContainsIf(v, [](auto i) { return (i == 55); }) ? std::cout << "Contains\n" : std::cout << "Not contains\n";
+  // (std::find_if(v.begin(), v.end(), [](auto i) { return (i == 55); }) != v.end()) ? std::cout << "Std Contains\n" : std::cout << "Std Not contains\n";
+  // ContainsIf(v, [](auto i) { return (i == 123); }) ? std::cout << "Contains\n" : std::cout << "Not contains\n";
+  // (std::find_if(v.begin(), v.end(), [](auto i) { return (i == 123); }) != v.end()) ? std::cout << "Std Contains\n" : std::cout << "Std Not contains\n";
+  // std::cout << "======================================================\n\n";
+
+  std::cout << "CountIf\n"
             << "======================================================\n";
-  std::vector<int> v = {10, 25, 40, 55};
-  Contains(v, 10) ? std::cout << "Contains\n" : std::cout << "Not contains\n";
-  (std::find(v.begin(), v.end(), 10) != v.end()) ? std::cout << "Contains\n" : std::cout << "Not contains\n";
+  std::vector<int> v = {1, 2, 2, 3, 4};
+  std::cout << CountIf(v, [](auto i) { return (i == 2); });
+  std::cout << CountIf(v, [](auto i) { return (i == 2); });
   std::cout << "======================================================\n";
 
+  // std::cout << "\n" << "======================================================\n";
+  // std::cout << "======================================================\n";
+
+  // std::cout << "\n" << "======================================================\n";
+  // std::cout << "======================================================\n";
+
+  // std::cout << "\n" << "======================================================\n";
+  // std::cout << "======================================================\n";
+
+  // std::cout << "\n" << "======================================================\n";
+  // std::cout << "======================================================\n";
+
+  // std::cout << "\n" << "======================================================\n";
+  // std::cout << "======================================================\n";
+
+  // std::cout << "\n" << "======================================================\n";
+  // std::cout << "======================================================\n";
 }
--- sp04/t00/utils/algorithmUtils.h
+++ sp04/t00/utils/algorithmUtils.h
@@ -3,46 +3,69 @@
 #include <algorithm>
 #include <iostream>
 
-// template <typename T>
-// bool IsInRange(const T& val, const T& from, const T& to) {
-// };
-
-// template <typename T>
-// bool IsInRange(const T& val, const std::pair<const T&, const T&>& minmax) {
-// };
-
 template <typename T>
-bool IsInsideRange(const T& val, const T& from, const T& to) {
+bool IsInRange(const T& val, const T& from, const T& to) {
   return (from <= val && val <= to);
 };
 
 template <typename T>
-bool IsInsideRange(const T& val, const std::pair<const T&, const T&>& minmax) {
+bool IsInRange(const T& val, const std::pair<const T&, const T&>& minmax) {
   return (minmax.first <= val && val <= minmax.second);
+};
+
+template <typename T>
+bool IsInsideRange(const T& val, const T& from, const T& to) {
+  return (from < val && val < to);
+};
+
+template <typename T>
+bool IsInsideRange(const T& val, const std::pair<const T&, const T&>& minmax) {
+  return (minmax.first < val && val < minmax.second);
 };
 
 template <class T, class U = T>
 bool Modify(T& obj, U&& new_value) {
   if (obj == new_value)
-    return true;
-  else {
-    obj = new_value;
-    return true;
-  }
-  return false;
+    return false;
+  obj = new_value;
+  return true;
 };
 
 template <class Collection, class T>
+void RemoveAll(Collection& c, const T& value) {
+  Collection new_c(std::begin(c),
+                   std::remove(std::begin(c), std::end(c), value));
+  c = new_c;
+}
+
+template <class Collection, class Pred>
+void RemoveAllIf(Collection& c, Pred&& predicate) {
+  Collection new_c(std::begin(c),
+                   std::remove_if(std::begin(c), std::end(c), predicate));
+  c = new_c;
+}
+
+template <class Collection, class T>
 auto Find(Collection& c, const T& value) {
-  return std::find(c.begin(), c.end(), value);
+  return std::find(std::begin(c), std::end(c), value);
 };
 
 template <class Collection, class Pred>
 auto FindIf(Collection& c, Pred&& predicate) {
-  return std::find_if(c.begin(), c.end(), predicate);
+  return std::find_if(std::begin(c), std::end(c), predicate);
 };
 
 template <class Collection, class T>
 bool Contains(const Collection& c, const T& value) {
-  return (std::find(c.begin(), c.end(), value) != c.end());
+  return (std::end(c) != Find(c, value)) ? true : false;
+};
+
+template <class Collection, class Pred>
+bool ContainsIf(const Collection& c, Pred&& predicate) {
+  return (std::end(c) != FindIf(c, predicate)) ? true : false;
+};
+
+template <class Collection, class Pred>
+int CountIf(const Collection& c, Pred&& predicate) {
+  return std::count_if(std::begin(c), std::end(c), predicate);
 };
