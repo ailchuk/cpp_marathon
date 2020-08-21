e029cfffafe1d05dece75c761eda10fb56eebafd
Andrii Ilchuk <ailchuk@e2r13p9.unit.ua>
2020-08-21 19:08:56 +0300

    t00 fin

--- sp04/t00/utils/algorithmUtils.h
+++ sp04/t00/utils/algorithmUtils.h
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <iostream>
+#include <numeric>
 
 template <typename T>
 bool IsInRange(const T& val, const T& from, const T& to) {
@@ -69,3 +70,82 @@ template <class Collection, class Pred>
 int CountIf(const Collection& c, Pred&& predicate) {
   return std::count_if(std::begin(c), std::end(c), predicate);
 };
+
+template <class T>
+const auto& Min(const T& arg) {
+  return arg;
+}
+
+template <class T, class... Args>
+const auto& Min(const T& arg, const Args&... args) {
+  return arg < Min(args...) ? arg : Min(args...);
+}
+
+template <class T>
+const auto& Max(const T& arg) {
+  return arg;
+}
+
+template <class T, class... Args>
+const auto& Max(const T& arg, const Args&... args) {
+  return arg > Max(args...) ? arg : Max(args...);
+}
+
+template <class Collection>
+auto MaxElement(const Collection& c) {
+  return std::max_element(std::begin(c), std::end(c));
+}
+
+template <class Collection>
+auto MinElement(const Collection& c) {
+  return std::min_element(std::begin(c), std::end(c));
+}
+
+template <class Collection>
+void Sort(Collection& c) {
+  std::sort(std::begin(c), std::end(c));
+}
+
+template <class Collection, class Comp>
+void Sort(Collection& c, Comp&& comparator) {
+  std::sort(std::begin(c), std::end(c), comparator);
+}
+
+// template <class Collection>
+// void Unique(Collection& c) {
+//   auto last = std::unique(std::begin(c), std::end(c));
+//   c.erase(last, std::end(c));
+//   std::sort(std::begin(c), std::end(c));
+//   last = std::unique(std::begin(c), std::end(c));
+//   c.erase(last,std::end(c));
+// }
+
+// template <class Collection, class Pred>
+// void Unique(Collection& c, Pred&& predicate) {
+//   std::unique(std::begin(c), std::end(c), predicate);
+// }
+
+template <class Collection, class Pred>
+void ForEach(Collection& c, Pred&& predicate) {
+  std::for_each(std::begin(c), std::end(c), predicate);
+}
+
+template <class Collection, class T>
+int IndexOf(const Collection& c, const T& value) {
+  int index = 0;
+
+  for (auto it = std::begin(c); it != std::end(c); ++it, ++index)
+    if (value == *it)
+      return index;
+  return -1;
+}
+
+template <class Collection, class Pred>
+int IndexOfIf(const Collection& c, Pred&& predicate) {
+  int index = 0;
+
+  for (auto it = std::begin(c); it != std::end(c); ++it, ++index)
+    if (predicate(*it))
+      return index;
+  return -1;
+}
