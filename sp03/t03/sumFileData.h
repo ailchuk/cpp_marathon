d21205ce254192a49ea556382dad0f20f3921e87
Andrii <46691148+ailchuk@users.noreply.github.com>
2020-08-21 18:09:18 +0300

    Update sumFileData.h

--- sp03/t03/sumFileData.h
+++ sp03/t03/sumFileData.h
@@ -3,9 +3,10 @@
 #include <fstream>
 #include <iterator>
 #include <string>
+#include <numeric>
 
 template <class T>
 T sumFileData(const std::string& fileName) {
   std::ifstream ss(fileName);
-  return std::accumulate(std::istream_iterator<T> begin(ss), {}, 0);
+  return std::accumulate(std::istream_iterator<T>(ss), {}, T(0));
 }
